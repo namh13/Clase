9516b808fc6d8d6f81bb761eabe388ef4897df3a
Alumno <alumno@localhost.localdomain>
2014-02-20 08:15:01 -0600

    Trabajo en Clase

---
+++ Clase/src/com/me/mygdxgame/clase.java
@@ -0,0 +1,22 @@
+package com.me.mygdxgame;
+
+class A
+{
+	private char x;
+	void setx(char x)
+	{
+		this.x=x;
+	}
+}
+
+
+public class clase {
+
+	public static void main(String[] args) {
+		
+		A a=new A();
+		a.setx('b');
+
+	}
+
+}
